c88d6d88cfb4a0f98dab9a992e8587936b3e9bd3
Verzul-1 <vipzenit666666@gmail.com>
2021-09-08 21:55:14 +0300

    First commit

---
+++ main/views.py
@@ -0,0 +1,12 @@
+from django.http.response import HttpResponse
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index (request):
+    return HttpResponse("<h4>Добрый вечер</h4>")
+
+def about(request):
+    return HttpResponse("<h4>Обо мне</h4>")
+
+def profile(request):
+    return HttpResponse("<h4>Профиль</h4>")
