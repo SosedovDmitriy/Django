59ad986cebd05bd0a0930018f948175b575504b1
Verzul-1 <vipzenit666666@gmail.com>
2021-09-09 19:20:04 +0300

    Фиксация

--- main/urls.py
+++ main/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('about', views.about),
+    path('', views.index, name='home'),
+    path('about', views.about, name='about'),
     path('profile', views.profile),
     path('test', views.test)
 ]
--- main/views.py
+++ main/views.py
@@ -1,12 +1,15 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def index (request):
-    return HttpResponse("<h4>Добрый вечер</h4>")
+    data = {
+        'title':'Главная страница',
+        'values': ['some', 'hello', '123']
+    }
+    return render(request, 'main/index.html', data)
 
 def about(request):
-    return HttpResponse("<h4>Обо мне</h4>")
+    return render(request, 'main/about.html')
 
 def profile(request):
     return HttpResponse("<h4>Профиль</h4>")
