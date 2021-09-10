48e2f233a7e91a99bc08a409b45d28f12379c6f9
SosedovDI <d.i.sosedov@gmail.com>
2021-09-10 15:18:48 +0300

    test first  commit

--- news/views.py
+++ news/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from datetime import datetime
 from .models import Articles
 
 def news_home(request):
+    #p = Articles(title='Dima', anons="HUI", full_text="asdasdasd",date=datetime.now())
+    #p.save()
     news=Articles.objects.all()
-    return render(request, 'news/news_home.html', {'news':news})
+    print(news)
+    return render(request, 'news/news_home.html', {"news":news})
